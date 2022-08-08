8a841675580b3d98c9d5618a46119b925426cc0c
Major_Tom49 <80781256+victor49100@users.noreply.github.com>
2022-08-08 22:04:50 +0200

    Update PdfConv.py

--- MangaDownloader/PdfConv.py
+++ MangaDownloader/PdfConv.py
@@ -1,6 +1,5 @@
+#import
 from PIL import Image
-
-
 
 
 def ConvertBerserk(formatF):
@@ -8,9 +7,7 @@ def ConvertBerserk(formatF):
     images_list = []
     for i in range(1, 350):
         try:
-            path = str(
-                #C:\Users\Victor\Desktop\MangaDownloader\Berserk\BerserkTome17\1.jpg
-                "C:/path/to/folder/Berserk/BerserkTome"+str(Tome)+"/"+str(i)+"."+str(formatF))
+            path = str("C:/path/to/folder/Berserk/BerserkTome"+str(Tome)+"/"+str(i)+"."+str(formatF))
 
             image = Image.open(str(path))
             im = image.convert('RGB')
@@ -31,8 +28,7 @@ def ConvertOp(formatF):
         
         try:
             if Langue == 1:
-                path = str(
-                    "C:/path/to/folder/OnePieceVf/OnePiece"+str(Tome)+"/"+str(i)+"."+str(formatF)) #if out of range pb change name
+                path = str("C:/path/to/folder/OnePieceVf/OnePiece"+str(Tome)+"/"+str(i)+"."+str(formatF)) #if out of range pb change name
             if Langue == 2:
                 path = str(
                     "C:/path/to/folder/OnePieceEn/OnePiece"+str(Tome)+"/"+str(i)+"."+str(formatF))
