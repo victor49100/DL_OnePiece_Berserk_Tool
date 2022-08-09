fe574fbb89e90c2c88a83fcd65732ff2c9007b7d
Major_Tom49 <80781256+victor49100@users.noreply.github.com>
2022-08-09 14:29:03 +0200

    Update Main.py

--- MangaDownloader/Main.py
+++ MangaDownloader/Main.py
@@ -1,16 +1,9 @@
 from OnePieceEnDl import DlOnePieceEn
 from OnePieceVfDl import DlOnePieceVf
+from PdfConv import Mainpdf
 from Berserk import DlBerserk
 
-def Main():
-    print (""" 
-   ____             _____ _                 ____                          _      _______          _ 
-  / __ \           |  __ (_)               |  _ \                        | |    |__   __|        | |
- | |  | |_ __   ___| |__) |  ___  ___ ___  | |_) | ___ _ __ ___  ___ _ __| | __    | | ___   ___ | |
- | |  | | '_ \ / _ \  ___/ |/ _ \/ __/ _ \ |  _ < / _ \ '__/ __|/ _ \ '__| |/ /    | |/ _ \ / _ \| |
- | |__| | | | |  __/ |   | |  __/ (_|  __/ | |_) |  __/ |  \__ \  __/ |  |   <     | | (_) | (_) | |
-  \____/|_| |_|\___|_|   |_|\___|\___\___| |____/ \___|_|  |___/\___|_|  |_|\_\    |_|\___/ \___/|_|
-    """)
+def dl():
     Tome = int(input("Voulez vous telechargé One Piece (1) ou Berserk (2) ? "))
     if Tome == 1:
         T = int (input("Entrez le Tome a téléchargé : "))
@@ -22,4 +15,23 @@ def Main():
     if Tome == 2: #berserk
         DlBerserk()
 
-Main()
+def pdf():
+    Mainpdf()
+
+
+print (""" 
+      ____             _____ _                 ____                          _     
+     / __ \           |  __ (_)               |  _ \                        | |    
+    | |  | |_ __   ___| |__) |  ___  ___ ___  | |_) | ___ _ __ ___  ___ _ __| | __ 
+    | |  | | '_ \ / _ \  ___/ |/ _ \/ __/ _ \ |  _ < / _ \ '__/ __|/ _ \ '__| |/ / 
+    | |__| | | | |  __/ |   | |  __/ (_|  __/ | |_) |  __/ |  \__ \  __/ |  |   <  
+     \____/|_| |_|\___|_|   |_|\___|\___\___| |____/ \___|_|  |___/\___|_|  |_|\_\ 
+    """)
+
+
+choice = int(input("Voulez vous telechargé (1) ou Convertir en PDF (2) ? "))
+if choice == 1:
+    dl()
+if choice == 2:
+    pdf()
+
