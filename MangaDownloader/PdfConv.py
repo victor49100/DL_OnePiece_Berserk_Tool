0f5d83ce5c38e89b1a7931eb2ac1c786738cfb58
Major_Tom49 <80781256+victor49100@users.noreply.github.com>
2022-08-09 14:29:33 +0200

    Add files via upload

--- MangaDownloader/PdfConv.py
+++ MangaDownloader/PdfConv.py
@@ -1,4 +1,3 @@
-#import
 from PIL import Image
 
 
@@ -7,7 +6,9 @@ def ConvertBerserk(formatF):
     images_list = []
     for i in range(1, 350):
         try:
-            path = str("C:/path/to/folder/Berserk/BerserkTome"+str(Tome)+"/"+str(i)+"."+str(formatF))
+            path = str(
+                #C:\Users\Victor\Desktop\MangaDownloader\Berserk\BerserkTome17\1.jpg
+                "C:/Users/Victor/Desktop/MangaDownloader/Berserk/BerserkTome"+str(Tome)+"/"+str(i)+"."+str(formatF))
 
             image = Image.open(str(path))
             im = image.convert('RGB')
@@ -28,10 +29,11 @@ def ConvertOp(formatF):
         
         try:
             if Langue == 1:
-                path = str("C:/path/to/folder/OnePieceVf/OnePiece"+str(Tome)+"/"+str(i)+"."+str(formatF)) #if out of range pb change name
+                path = str(
+                    "C:/Users/Victor/Desktop/MangaDownloader/OnePieceVf/OnePiece"+str(Tome)+"/"+str(i)+"."+str(formatF)) #if out of range pb change name
             if Langue == 2:
                 path = str(
-                    "C:/path/to/folder/OnePieceEn/OnePiece"+str(Tome)+"/"+str(i)+"."+str(formatF))
+                    "C:/Users/Victor/Desktop/MangaDownloader/OnePieceEn/OnePiece"+str(Tome)+"/"+str(i)+"."+str(formatF))
             image = Image.open(str(path))
             im = image.convert('RGB')
             images_list.append(im)
@@ -41,10 +43,12 @@ def ConvertOp(formatF):
             print("PDF de "+str(i) + " pages généré")
             break
 
-choix = int(input("Convertir One Piece(1) Ou berserk(2) ? "))
-formatF = str(input("quelle est le format du fichier ex(png,jpeg,webp ...) ? "))
- 
-if choix == 2:
-    ConvertBerserk(formatF)
-if choix == 1:
-    ConvertOp(formatF)
+
+def Mainpdf():
+    choix = int(input("Convertir One Piece(1) Ou berserk(2) ? "))
+    formatF = str(input("quelle est le format du fichier ex(png,jpeg,webp ...) ? "))
+
+    if choix == 2:
+        ConvertBerserk(formatF)
+    if choix == 1:
+        ConvertOp(formatF)
